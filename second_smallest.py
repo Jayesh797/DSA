31a9dbcc8a8b31f566bf93d0b0ebda43fc3457fa
Jayesh797 <160374127+Jayesh797@users.noreply.github.com>
2024-04-18 17:34:25 +0530

    second smallest element

---
+++ second_smallest.py
@@ -0,0 +1,14 @@
+def find_second_smallest(arr):
+    smallest=arr[0]
+    ssmallest=10000
+    for i in range(1,len(arr)):
+        if(smallest>arr[i]):
+            ssmallest=smallest
+            smallest=arr[i]
+        elif(arr[i]!=smallest and arr[i]<ssmallest):
+            ssmallest=arr[i]
+    return ssmallest
+
+second_smallest=find_second_smallest([2,3,1,4,7,5])
+print(second_smallest)
+
