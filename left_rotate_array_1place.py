c89f5d6f46155a77c57b868789f79ba8958e18c3
Jayesh797 <160374127+Jayesh797@users.noreply.github.com>
2024-04-19 14:04:46 +0530

    move element by left

---
+++ left_rotate_array_1place.py
@@ -0,0 +1,16 @@
+def rotate_array_by_1(arr):
+    # temp=arr[0]
+    # for i in range(1,len(arr)):
+    #     arr[i-1]=arr[i]
+    # arr[len(arr)-1]=temp
+    # return arr
+
+    for i in range(1,len(arr)):
+        temp=arr[i-1]
+        arr[i-1]=arr[i]
+        arr[i]=temp
+    return arr
+
+
+arr=rotate_array_by_1([1,2,3,4,5])
+print(arr)
