6de5d48cd4ee402579e3ef41d1e20fdbb8a7aea5
Jayesh797 <160374127+Jayesh797@users.noreply.github.com>
2024-05-19 12:55:32 +0530

    n_permutations

---
+++ n_permutations.py
@@ -0,0 +1,31 @@
+def n_permutations(nums):
+    n=len(nums)
+    i=-1
+    j=n-1
+    while(j>=0):
+        if(nums[j-1]<nums[j]):
+            i=j-1
+            break
+        j-=1
+    if(i==-1):
+        nums2=sorted(nums)
+        return nums2
+    k=n-1
+    while(k>=0):
+        if(nums[k]>nums[i]):
+            nums[k],nums[i]=nums[i],nums[k]
+            break
+        k-=1
+    z=n-1
+    s=i+1
+    while(s<z):
+        nums[s],nums[z]=nums[z],nums[s]
+        s+=1
+        z-=1
+    
+    return nums
+
+arr=[1,2,3,4,5]
+sd=n_permutations(arr)
+print(sd)
+    
