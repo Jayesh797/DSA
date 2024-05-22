3ee7cfd486bc557920759470198ff2b64b0cf3e2
Jayesh797 <160374127+Jayesh797@users.noreply.github.com>
2024-05-22 14:48:26 +0530

    updated code

--- set_matrix_zero.py
+++ set_matrix_zero.py
@@ -1,18 +1,20 @@
-def matrix_zero(arr,m,n):
-    col=[0]*m
-    row=[0]*n
-    for i in range(0,m):
-        for j in range(0,n):
-            if(arr[i][j]==0):
-                row[i]=1
-                col[j]=1
+def setZeroes(matrix):
+        r=len(matrix)
+        c=len(matrix[0])
+        row=[0]*r
+        col=[0]*c
+        for i in range(0,r):
+            for j in range(0,c):
+                if(matrix[i][j]==0):
+                    row[i]=1
+                    col[j]=1
         
-    for i in range(0,m):
-        for j in range(0,n):
-            if(col[j]==1 or row[i]==1):
-                arr[i][j]=0
-    
-    return arr
+        for i in range(0,r):
+            for j in range(0,c):
+                if(row[i]==1 or col[j]==1):
+                    matrix[i][j]=0
+        
+        return matrix
 
 arr=[
     [1, 1, 1, 1],
@@ -23,7 +25,7 @@ arr=[
 
 
 
-sd=matrix_zero(arr,4,4)
+sd=setZeroes(arr)
 
 for i in range(0,4):
     for j in range(0,4):
