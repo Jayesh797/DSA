b7ba795959625ddbafc41884eadcb620a6800935
Jayesh797 <160374127+Jayesh797@users.noreply.github.com>
2024-06-07 16:22:11 +0530

    recursion preactise

---
+++ recursion_practise.py
@@ -0,0 +1,29 @@
+#print names five times using recursion
+# def print_names(n):
+#     if(n==0):
+#         return
+#     else:
+#         print('vijay')
+#         print_names(n-1)
+
+# print_names(5)
+
+#print lineraly 1 to N
+# def print_n(i,n):
+#     if(n==0):
+#         return 
+#     else:
+#         print(i)
+#         print_n(i+1,n-1)
+
+# print_n(1,6)
+
+#print lineraly N to 1
+# def print_n_rev(n):
+#     if(n==0):
+#         return 
+#     else:
+#         print(n)
+#         print_n_rev(n-1)
+
+# print_n_rev(6)
